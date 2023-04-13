17b032edf0b55514f202bc7c2f3ab453bda3f27b
hrsk <hrsk.yegor@gmail.com>
2023-04-13 23:49:37 +0300

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 export const App = () => {
     return (
         <div className="App">
+            <h1>
+                TODOLIST
+            </h1>
         </div>
     );
 }

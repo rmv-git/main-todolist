61d8434ebd2378453e01116cba788bd3735875d0
hrsk <hrsk.yegor@gmail.com>
2023-07-23 20:44:52 +0300

    app reducer tests, app status action

--- src/store/app-reducer.ts
+++ src/store/app-reducer.ts
@@ -1,21 +1,30 @@
 const initialState: InitialStateType = {
     error: null,
+    status: 'idle',
 }
 
-export const appReducer = (state: InitialStateType = initialState, action: SetAppErrorActionType): InitialStateType => {
+export const appReducer = (state: InitialStateType = initialState, action: AppReducerActionType): InitialStateType => {
     switch (action.type) {
         case 'SET-ERROR':
             return {...state, error: action.error}
+        case 'SET_STATUS':
+            return {...state, status: action.status}
         default:
             return {...state}
     }
 }
 
+export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed';
+
 export type InitialStateType = {
-    // если ошибка какая-то глобальная произойдёт - мы запишем текст ошибки сюда
-    error: string | null
+    error: string | null,
+    status: RequestStatusType,
 }
 
-export const setAppErrorAC = (error: string | null) => ({type: 'SET-ERROR', error} as const)
+export const setAppErrorAC = (error: string | null) => ({type: 'SET-ERROR', error} as const);
+export const setAppStatusAC = (status: RequestStatusType) => ({type: 'SET_STATUS', status} as const);
+
 
 export type SetAppErrorActionType = ReturnType<typeof setAppErrorAC>;
+export type SetAppStatusActionType = ReturnType<typeof setAppStatusAC>;
+type AppReducerActionType = SetAppErrorActionType | SetAppStatusActionType;

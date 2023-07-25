1bf00992cf4cd0b36b2edbc5330f063ef6f111da
hrsk <hrsk.yegor@gmail.com>
2023-07-25 19:04:35 +0300

    handleServerAppError

---
+++ src/utils/error-utils.ts
@@ -0,0 +1,16 @@
+import {Dispatch} from 'redux'
+import {setAppErrorAC, SetAppErrorActionType, setAppStatusAC, SetAppStatusActionType} from "../store/app-reducer";
+
+export const handleServerAppError = <T>(data:any, dispatch: Dispatch<SetAppErrorActionType | SetAppStatusActionType>) => {
+    if (data.messages.length) {
+        dispatch(setAppErrorAC(data.messages[0]))
+    } else {
+        dispatch(setAppErrorAC('Some error occurred'))
+    }
+    dispatch(setAppStatusAC('failed'))
+}
+
+export const handleServerNetworkError = (error: { message: string }, dispatch: Dispatch<SetAppErrorActionType | SetAppStatusActionType>) => {
+    dispatch(setAppErrorAC(error.message ? error.message : 'Some error occurred'))
+    dispatch(setAppStatusAC('failed'))
+}
